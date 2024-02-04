14a6a052ac2d464d298030d1cb56de7ef6294bc4
ShobiyaL <sofy7468612@gmail.com>
2024-02-04 15:14:48 +0530

    services

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -8,13 +8,22 @@ const ServiceCard = ({ title, component }) => {
   useEffect(() => {
     let ctx = gsap.context(() => {
       const tl = gsap.timeline(); //{ duration: 2, x: 100, opacity: 0.5 }
-      tl.to('.service-card', { x: 100, duration: 2, opacity: 0.5 });
+      tl.to('.service-card', {
+        x: 50,
+        duration: 2,
+        opacity: 0.5,
+        delay: 0.5,
+        stagger: {
+          each: 0.5,
+          from: 'random',
+        },
+      });
     }, component);
     return () => ctx.revert();
   }, [component]);
   return (
     <Tilt className='xs:w-[250px] w-[250px]'>
-      <div className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-md service-card'>
+      <div className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-md  service-card'>
         <div
           className='bg-slate-900 rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly 
           items-center flex-col '
